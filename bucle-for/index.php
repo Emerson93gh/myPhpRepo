05bae11a976da0eef740afe43890fefd8f9d3aca
Emerson Solano <emerson-solano@hotmail.com>
2022-07-02 10:36:52 -0600

    bucles

---
+++ bucle-for/index.php
@@ -0,0 +1,12 @@
+<?php
+// FOR
+
+// Sumar del 1 al 100
+$resultado = 0;
+
+for($i = 0; $i <= 100; $i++){
+    $resultado = $resultado + $i;
+}
+echo "El resultado es: " . $resultado;
+
+?>
--- bucles/index.php
+++ bucles/index.php
@@ -2,9 +2,38 @@
 // Bucle while
 $numero = 0;
 
-while ($numero <= 100) {
-    echo "<p>$numero</p>";
-    $numero++;
+// while ($numero <= 100) {
+//     echo "<p>$numero</p>";
+//     $numero++;
+// }
+
+if (isset($_GET['numero'])) {
+    // cambiar tipo de dato
+    $numero = (int)$_GET['numero'];
+} else {
+    $numero = 1;
 }
 
+echo "<h1>Tabla de multiplicar del numero $numero</h1>";
+
+$contador = 1;
+while($contador <= 10) {
+    echo "$numero x $contador = ".($numero*$contador)."<br/>";
+    $contador++;
+}
+
+echo "<hr/>";
+// Do While
+$edad = 17;
+$contador = 1;
+do{
+    //instrucciones
+    echo "Tienes acceso al local privado $contador <br/>";
+    $contador++;
+} while($edad >= 18 && $contador <= 10);
+
+// BREAK
+/*
+break; puede ir dentro de los bucles para hacer pruebas o finalizar el bucle al no cumplir una condicion
+*/
 ?>
