7537a67ed3f0b85c4f379693d981a23dbdbd9d33
Emerson Solano <emerson-solano@hotmail.com>
2022-07-06 15:12:17 -0600

    sql avance 2

--- sql/02-modificar-tablas/modificar.sql
+++ sql/02-modificar-tablas/modificar.sql
@@ -6,3 +6,12 @@ ALTER TABLE usuarios_renom CHANGE apellidos apellido varchar(100) null;
 
 /* MODIFICAR COLUMNA SIN CAMBIAR NOMBRE */
 ALTER TABLE usuarios_renom MODIFY apellido char(50) not null;
+
+/* AGREGAR UNA COLUMNA */
+ALTER TABLE usuarios_renom ADD website varchar(100) null;
+
+/* AGREGAR RESTICCION A COLUMNA */
+ALTER TABLE usuarios_renom ADD CONSTRAINT uq_email UNIQUE(email);
+
+/* BORRAR UNA COLUMNA, aunque contenga datos */
+ALTER TABLE usuarios_renom DROP website;
