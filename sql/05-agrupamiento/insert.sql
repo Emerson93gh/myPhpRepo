623a427aa8bae59c064824d1e3e84f38a0760c8e
Emerson Solano <emerson-solano@hotmail.com>
2022-07-24 18:39:15 -0600

    sql avance 5

---
+++ sql/05-agrupamiento/insert.sql
@@ -0,0 +1,17 @@
+/* Inserts para categorias */
+INSERT INTO categorias VALUES (null, 'Accion');
+INSERT INTO categorias VALUES (null, 'Rol');
+INSERT INTO categorias VALUES (null, 'Deportes');
+
+/* Inserts para entradas */
+INSERT INTO entradas VALUES (null, 1, 1, 'Novedades GTA 6', 'Review 2023', CURDATE());
+INSERT INTO entradas VALUES (null, 1, 2, 'Novedades ROL GAME', 'Review completa', CURDATE());
+INSERT INTO entradas VALUES (null, 1, 3, 'Nuevos jugadores', 'Nuevas jugadas maestras', CURDATE());
+
+INSERT INTO entradas VALUES (null, 2, 1, 'Novedades 7DS', 'Review 2023', CURDATE());
+INSERT INTO entradas VALUES (null, 2, 2, 'Novedades LOL', 'Review completa II', CURDATE());
+INSERT INTO entradas VALUES (null, 2, 3, 'Veteranos jugadores', 'Jugadas clasicas', CURDATE());
+
+INSERT INTO entradas VALUES (null, 4, 1, 'Novedades FailGuys', 'Review 2023', CURDATE());
+INSERT INTO entradas VALUES (null, 4, 2, 'Novedades COD9', 'Review completa', CURDATE());
+INSERT INTO entradas VALUES (null, 4, 3, 'Jugadores Leyenda', 'Jugadas imposibles', CURDATE());
