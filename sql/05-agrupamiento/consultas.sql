50c1d25ae295496bf789a8aa30c5f69bc6548287
Emerson Solano <emerson-solano@hotmail.com>
2022-08-21 09:54:17 -0600

    sql agrupamiento

--- sql/05-agrupamiento/consultas.sql
+++ sql/05-agrupamiento/consultas.sql
@@ -1,1 +1,21 @@
 /* Consultas de agrupamiento */
+SELECT titulo FROM entradas GROUP BY categoria_id;
+
+/* mostrar una columna con el numero de entradas por cada categoria (agrupamiento) */
+SELECT COUNT(titulo) AS 'NUMERO ENTRADAS', categoria_id FROM entradas GROUP BY categoria_id;
+
+/* Consulta de agrupamiento con condicion */
+SELECT COUNT(titulo) AS 'NUMERO ENTRADAS', categoria_id FROM entradas GROUP BY categoria_id HAVING COUNT(titulo)>= 2;
+
+/* FUNCIONES DE AGRUPAMIENTO
+AVG     Sacar la media
+COUNT   Contar el numero de elementos
+MAX     Valor maximo del grupo
+MIN     Valor minimo del grupo
+SUM     Sumar todo el contenido del grupo
+*/
+
+SELECT AVG(id) AS 'Media de entradas' FROM entradas;
+SELECT MAX(id) AS 'Maximo id', titulo FROM entradas;
+SELECT MIN(id) AS 'Minimo id', titulo FROM entradas;
+SELECT SUM(id) AS 'Suma de id', titulo FROM entradas;
