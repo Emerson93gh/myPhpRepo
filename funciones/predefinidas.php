6841322114dd561d724e02fa05fd894744821408
Emerson Solano <emerson-solano@hotmail.com>
2023-01-23 21:56:33 -0600

    funciones predefinidas 1

---
+++ funciones/predefinidas.php
@@ -0,0 +1,19 @@
+<?php
+
+// Debugguear
+$nombre = "Alexander";
+var_dump($nombre);
+echo "<br/>";
+// Fechas
+echo date('d-m-Y');
+echo "<br/>";
+echo time();
+// Matematicas
+echo "<br/>";
+echo "La raiz cuadrada de 10: " . sqrt(10);
+echo "<br/>";
+echo "Numero aleatorio entre 10 y 40: " .rand(10, 40);
+echo "<br/>";
+echo "Numero pi: " .pi();
+echo "<br/>";
+echo "Redondear: " .round(7.819453); //mas precision a 2 decimales = .round(7.819453, 2);
